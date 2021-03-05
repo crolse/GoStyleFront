406b6f0f5e6701188d9b2deba513e154f1ee015d
gwendal8577 <gwendal.mains@gmail.com>
2021-03-05 13:20:55 +0100

    v0

---
+++ vuejs/src/components/PromotionCodeTable.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="PromotionCodeTable">
+    <table>
+      <thead>
+        <tr>
+          <th class="column">Nom</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="promotion in promotions.data" :key="promotion.id">
+          <router-link :to="'/details/' + promotion.name">
+            <!--          todo-->
+            <td>
+              {{ promotion.name }}
+            </td>
+          </router-link>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "PromotionCodeTable",
+  data() {
+    return {
+      promotions: []
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    getList() {
+      axios
+        // todo
+        .get(" ")
+        .then(response => (this.promotions = response.data));
+    }
+  }
+};
+</script>
+
+<style scoped>
+table {
+  border-collapse: collapse;
+}
+td {
+  border: solid 1px #808080;
+}
+</style>
---
+++ vuejs/src/views/PromotionCode.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="PromotionCodeTable">
+  </div>
+</template>
+
+<script>
+export default {
+name: "PromotionCode"
+}
+</script>
+
+<style scoped>
+
+</style>
