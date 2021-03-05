5f9c321e889492ba3a6a7dc9630e87d6635760f6
fabienD <fabien.drapeau1@epsi.fr>
2021-03-05 14:46:32 +0100

    feature added  header

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
+    <v-app-bar app color="red" dark>
+      <v-spacer></v-spacer>
+        <v-img justify=""
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
@@ -10,29 +10,16 @@
           transition="scale-transition"
           width="40"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
+        <v-spacer></v-spacer>
+        <v-btn
+        v-if="show_Button"
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
-
     <v-main>
       <HelloWorld />
     </v-main>
@@ -48,9 +35,12 @@ export default {
   components: {
     HelloWorld
   },
-
   data: () => ({
+            show_Button: false,
+  
     //
   })
 };
 </script>
+
+
