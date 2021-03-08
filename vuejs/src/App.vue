a43855c17c3b08db627c76946573a44d48214b8e
Bastien CARCAT <bastien-carcat@orange.fr>
2021-03-08 15:35:44 +0100

    fix: missing </script> due to merge

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -2,24 +2,25 @@
   <v-app>
     <v-app-bar app color="red" dark>
       <v-spacer></v-spacer>
-      <v-img justify=""
-      alt="Vuetify Logo"
-      class="shrink mr-2"
-      contain
-      src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-      transition="scale-transition"
-      width="40"
+      <v-img
+        justify=""
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
       />
       <v-spacer></v-spacer>
       <v-btn
-      v-if="show_Button"
-      href="https://github.com/vuetifyjs/vuetify/releases/latest"
-      target="_blank"
-      text
+        v-if="show_Button"
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
       >
-      <v-icon>mdi-open-in-new</v-icon>
-    </v-btn>
-  </v-app-bar>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
     <v-main>
       <!-- <v-container fluid> -->
       <router-view />
@@ -36,5 +37,4 @@ export default {
     show_Button: false,
   }),
 };
-
-
+</script>
